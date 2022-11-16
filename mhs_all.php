a5c91eaa57f767b552aae3ca28e05a511cda6de2
Nurul Uswatun Hasanah <nuruluss19@gmail.com>
2022-11-17 05:45:18 +0700

    folder 4

---
+++ mhs_all.php
@@ -0,0 +1,27 @@
+	
+				<tbody>
+					<?php
+					include "../koneksi.php";
+					session_start();
+					
+						$querymhs = mysqli_query ($konek, "SELECT NIM, Nama_Mahasiswa FROM mahasiswa");
+						if($querymhs == false){
+							die ("Terjadi Kesalahan : ". mysqli_error($konek));
+						}
+							
+						while ($mhs = mysqli_fetch_array ($querymhs)){
+							
+							echo "
+								<tr>
+									<td style=\"width: 348px;\">$mhs[NIM]</td>
+									<td style=\"width: 594px;\">$mhs[Nama_Mahasiswa]</td>
+									<td>
+										<center>
+										<a href='#' class='open_lihat' id='$mhs[NIM]'>Lihat</a>
+										</center>
+									</td>
+								</tr>";
+						}
+						include('script_lihat.php');
+					?>
+				</tbody>
