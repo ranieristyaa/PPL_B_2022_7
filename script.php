8e2bb21458d96fba70f180569362ef35d1994cdd
unknown <fikri.afrizal02@gmail.com>
2022-11-17 10:06:59 +0700

    login

---
+++ script.php
@@ -0,0 +1,10 @@
+<!-- Javascript Edit--> 
+<script type="text/javascript">
+		$(document).ready(function () {
+            $("#form").submit(function(e) {
+                var success = document.getElementById('success');
+                success.style.display = 'visible';
+				$("#success").html('NIM/NIP berhasil disubmit, tunggu 1x24 jam hingga Anda dapat menggunakan akun Anda.');
+			});
+        });
+</script>
